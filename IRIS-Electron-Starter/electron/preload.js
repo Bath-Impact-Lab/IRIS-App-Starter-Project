72a3e39e84510d53d503314cb6c5d3850f7fc488
Dr Chris Egwim <emeksense@gmail.com>
2025-10-21 11:52:24 +0100

    remove unsued project files and related assets

--- IRIS-Electron-Starter/electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  resolveAsset: async (relPath) => ipcRenderer.invoke('resolve-asset', relPath),
-  readAsset: async (relPath) => ipcRenderer.invoke('read-asset', relPath)
-});
